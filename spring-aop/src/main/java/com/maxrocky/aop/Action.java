ac246575ac4abdc50b22d122ee06671d232ebc43
zhuyadong222 <zhuyadong222@foxmail.com>
2018-05-11 17:26:05 +0800

    aop包中有自定义注解

--- spring-aop/src/main/java/com/maxrocky/aop/Action.java
+++ spring-aop/src/main/java/com/maxrocky/aop/Action.java
@@ -5,9 +5,15 @@ import java.lang.annotation.*;
 /**
  * @author yado
  * @create 2018-05-04 9:50
- * @desc    编写拦截规则的注解
+ * @desc    自定义注解
+ *          编写拦截规则的注解
  *          注解本身是没有功能的，和xml文件一样，都是元数据，即解释数据的数据，就是所谓的配置文件
  **/
+
+/**
+ *  相关介绍查看笔记"java自定义注解"
+ */
+
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
